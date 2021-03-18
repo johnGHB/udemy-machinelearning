c42756e7845d10a0e8db0f221434473f63ae66b3
JohnGHB <johnn.mail@icloud.com>
2021-03-18 14:14:27 -0500

    Machine Learning Section 3.21

--- data_processing_tool.py
+++ data_processing_tool.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 dataset = pd.read_csv('/Users/JohnMB2011/Downloads/Machine_Learning_Code_Datasets/Part1_Data_Preprocessing/Section2/Python/Data.csv')
 # include all rows and all columns except the last column
@@ -10,3 +11,9 @@ y = dataset.iloc[:, -1].values
 
 print(x)
 print(y)
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+print(x)
